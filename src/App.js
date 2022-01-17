c43fe41c76d6b45f725e22e7f63e1ad4f31d395f
Mankirat <mankirat95@gmail.com>
2022-01-17 20:47:53 +0530

    Contact Object added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
 import React, { Component } from 'react'
-
 class App extends Component {
+    contact = [
+        {
+          id: 'ryan',
+          name: 'Ryan Florence',
+          email: 'ryan@reacttraining.com',
+        //   avatarURL: config.origin + '/ryan.jpg'
+        },
+        {
+          id: 'michael',
+          name: 'Michael Jackson',
+          email: 'michael@reacttraining.com',
+        //   avatarURL: config.origin + '/michael.jpg'
+        },
+        {
+          id: 'tyler',
+          name: 'Tyler McGinnis',
+          email: 'tyler@reacttraining.com',
+        //   avatarURL: config.origin + '/tyler.jpg'
+        }
+      ]
     render() {
         return (
             <div>
-                hello
+                
             </div>
         )
     }
