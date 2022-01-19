11bfe6123410a85e6404057be7dc6248c43746d3
Mankirat <mankirat95@gmail.com>
2022-01-19 22:24:37 +0530

    State changes done in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react'
-import {CONTACTS} from './Components/contacts';
-import ListContacts from './Components/ListContacts';
-import SearchContacts from './Components/SearchContacts';
+import React, { Component } from "react";
+import { CONTACTS } from "./Components/contacts";
+import ListContacts from "./Components/ListContacts";
+import SearchContacts from "./Components/SearchContacts";
 class App extends Component {
-    state = {
-        contacts:CONTACTS,
-    }
-    removeContact = this.removeContact.bind(this);
-    removeContact(contact){
-        this.setState((state)=>({
-            contacts: state.contacts.filter(c=>c.id!==contact.id)
-        }))
-    }
-    render() {
-        return (
-            <>
-                <SearchContacts></SearchContacts>
-                <ListContacts contacts = {this.state.contacts} remove={this.removeContact}></ListContacts>
-            </>
-        )
-    }
+  state = {
+    contacts: CONTACTS,
+  };
+  removeContact = this.removeContact.bind(this);
+  removeContact(contact) {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id),
+    }));
+  }
+  render() {
+    return (
+      <>
+        <SearchContacts contacts={this.state.contacts}></SearchContacts>
+        <ListContacts
+          contacts={this.state.contacts}
+          remove={this.removeContact}
+        ></ListContacts>
+      </>
+    );
+  }
 }
 export default App;
