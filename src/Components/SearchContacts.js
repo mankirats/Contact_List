2fb0c19b70dc04242cd94b0f8f366c5fab7b8f18
Mankirat <mankirat95@gmail.com>
2022-01-19 22:23:57 +0530

    SearchComponent state new functions added

--- src/Components/SearchContacts.js
+++ src/Components/SearchContacts.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 
 function SearchContacts(props) {
-  const [searchResult, searchContact] = useState("");
+  let [searchResult, setSearchResult] = useState("");
+
   const handleSearch = (event) => {
-    searchContact((prevState) => {
-      return { ...prevState, searchResult: event.target.value };
-    });
+    setSearchResult((searchResult = event.target.value));
+    searchedContacts();
+  };
+  const searchedContacts = () => {
+    let re = new RegExp(searchResult, "gi");
+    let result = props.contacts
+      .map((c) => re.exec(c.name))
+      .filter((result) => result != null && result["input"] != null)
+      .map((r) => r["input"]);
+    console.log(result);
+    return result;
   };
   return (
     <>
-      <input type="text" value={searchResult} onChange={() => handleSearch()} />
+      <input type="text" value={searchResult} onChange={handleSearch} />
     </>
   );
 }
