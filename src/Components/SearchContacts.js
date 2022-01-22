9c97049bd3ae367792f83cb3d8c3287900e46e3e
Mankirat <mankirat95@gmail.com>
2022-01-22 22:20:01 +0530

    Search Component fixed

--- src/Components/SearchContacts.js
+++ src/Components/SearchContacts.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 
 function SearchContacts(props) {
   let [searchInput, setSearchInput] = useState("");
+  handleInputChange = (event) => {
+    setSearchInput((searchInput = event.target.value));
+    console.log(searchInput);
+  };
   return (
     <>
       <input
         type="text"
         value={searchInput}
-        onChange={(event) => setSearchInput((searchInput = event.target.value))}
+        onChange={(event) => handleInputChange(event)}
       />
     </>
   );
