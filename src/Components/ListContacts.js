af830bc62b7750e402379460b74d251a4a006f3f
Mankirat <mankirat95@gmail.com>
2022-01-23 16:00:38 +0530

    List component styled components added

--- src/Components/ListContacts.js
+++ src/Components/ListContacts.js
@@ -2,6 +2,44 @@ import React, { useState } from "react";
 import propTypes from "prop-types";
 import SearchContacts from "./SearchContacts";
 import escapeStringRegexp from "escape-string-regexp";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  padding: 1rem;
+`;
+
+const ContactList = styled.ul`
+  padding: 0;
+`;
+
+const ContactItems = styled.li`
+  position: relative;
+  border: 0.1px solid #474747;
+  border-radius: 4px;
+  list-style-type: none;
+  width: 100%;
+  padding: 0.5rem 1.5rem;
+  margin-top: 5px;
+  font-size: 1.05rem;
+`;
+
+const RemoveContactButton = styled.button`
+  position: absolute;
+  right: 6px;
+  bottom: 4.5px;
+  border-radius: 4px;
+  background-color: #474747;
+  color: white;
+  border: transparent;
+  outline: none;
+  padding: 0.35rem;
+  margin-right: auto;
+`;
+
+const ContactCounts = styled.div`
+  padding: 1rem 0rem 0rem 0.5rem;
+  text-align: center;
+`;
 
 function ListContacts(props) {
   let [contactSearch, setContactSearch] = useState("");
@@ -19,19 +57,24 @@ function ListContacts(props) {
   }
 
   return (
-    <>
+    <PageContainer>
       <SearchContacts
         liftingSearchInput={(val) => handlingSearch(val)}
       ></SearchContacts>
-      <ul>
+      <ContactCounts>
+        Now showing {showingContacts.length} of {props.contacts.length} Contacts
+      </ContactCounts>
+      <ContactList>
         {showingContacts.map((contact) => (
-          <li key={contact.id}>
+          <ContactItems key={contact.id}>
             {contact.name}
-            <button onClick={() => props.remove(contact)}>REMove</button>
-          </li>
+            <RemoveContactButton onClick={() => props.remove(contact)}>
+              REMOVE
+            </RemoveContactButton>
+          </ContactItems>
         ))}
-      </ul>
-    </>
+      </ContactList>
+    </PageContainer>
   );
 }
 
