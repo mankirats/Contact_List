35732858b4254bf9e61e74ed74a09a2612887261
Mankirat <mankirat95@gmail.com>
2022-01-23 18:55:43 +0530

    ShowAll button added

--- src/Components/ListContacts.js
+++ src/Components/ListContacts.js
@@ -3,42 +3,59 @@ import propTypes from "prop-types";
 import SearchContacts from "./SearchContacts";
 import escapeStringRegexp from "escape-string-regexp";
 import styled from "styled-components";
+import { device } from "../device";
 
 const PageContainer = styled.div`
   padding: 1rem;
+  height: 100vh;
+  margin-left: auto;
+  margin-right: auto;
+  @media ${device.laptop} {
+    max-width: 50%;
+  }
 `;
 
 const ContactList = styled.ul`
   padding: 0;
 `;
 
+const ShowAllButton = styled.a``;
+
 const ContactItems = styled.li`
+  background-color: #f7f7f8;
   position: relative;
   border: 0.1px solid #474747;
   border-radius: 4px;
   list-style-type: none;
   width: 100%;
-  padding: 0.5rem 1.5rem;
+  letter-spacing: 0.5px;
+  padding: 0.5rem 1rem;
   margin-top: 5px;
   font-size: 1.05rem;
 `;
 
 const RemoveContactButton = styled.button`
   position: absolute;
-  right: 6px;
-  bottom: 4.5px;
+  right: 10px;
+  bottom: 3.5px;
   border-radius: 4px;
-  background-color: #474747;
-  color: white;
+  background-color: transparent;
+  color: #b00020;
+  font-weight: 600;
+  font-size: 0.9rem;
   border: transparent;
   outline: none;
   padding: 0.35rem;
   margin-right: auto;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 const ContactCounts = styled.div`
   padding: 1rem 0rem 0rem 0.5rem;
   text-align: center;
+  font-weight: 500;
 `;
 
 function ListContacts(props) {
@@ -48,6 +65,11 @@ function ListContacts(props) {
     setContactSearch((contactSearch = searchKey));
     console.log(contactSearch);
   };
+
+  const showAllContacts = () => {
+    setContactSearch((contactSearch = ""));
+  };
+
   let showingContacts;
   if (contactSearch) {
     const match = new RegExp(escapeStringRegexp(contactSearch), "i");
@@ -62,7 +84,14 @@ function ListContacts(props) {
         liftingSearchInput={(val) => handlingSearch(val)}
       ></SearchContacts>
       <ContactCounts>
-        Now showing {showingContacts.length} of {props.contacts.length} Contacts
+        Now showing {showingContacts.length} of {props.contacts.length} Contacts{" "}
+        {showingContacts.length < props.contacts.length ? (
+          <ShowAllButton onClick={() => showAllContacts()}>
+            Show All
+          </ShowAllButton>
+        ) : (
+          <></>
+        )}
       </ContactCounts>
       <ContactList>
         {showingContacts.map((contact) => (
